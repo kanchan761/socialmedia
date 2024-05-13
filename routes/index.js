4452e292554169a1c6b4f4e23235e6a708a939f8
Your Name <you@example.comkanchanvishwakarma555555@gmail.com>
2024-05-13 20:18:59 +0530

    next

--- routes/index.js
+++ routes/index.js
@@ -24,14 +24,6 @@ failureRedirect :"/login",
  
 });
 
-function isloggedIn(req,res,next){
-  if(req.isAuthenticated){
-    next()
-  }
-  else{
-    res.redirect("/login")
-  }
-}
 
 router.get("/logout",function(req,res,next){
   req.logout(()=>{
@@ -43,28 +35,56 @@ router.get('/register', function(req, res, next) {
   res.render("resgister",{user : req.user});
 });
 
-
 router.post('/register-user', async function(req, res, next) {
-      try{
-        // const newuser = new user(req.body)
-        // await newuser.save()
+  try{
+    // const newuser = new user(req.body)
+    // await newuser.save()
         // res.redirect("/login")
         const {name,username ,email,password} = req.body
-         await user.register({name,username,email}, password)
+        await user.register({name,username,email}, password)
         res.redirect("/login")
-}
-      catch(error){
-          res.send(error.message)
       }
-});
+      catch(error){
+        res.send(error.message)
+      }
+}); 
 
-router.get('/profile', isloggedIn, function(req, res, next) {
+router.get('/profile', isLoggedIn, function(req, res, next) {
   res.render("profile",{user : req.user});
 });
 
-router.get('/update-user/:id', isloggedIn, function(req, res, next) {
+router.get('/update-user/:id', isLoggedIn, function(req, res, next) {
   res.render("update",{user : req.user});
 });
 
+router.get('/reset-password/:id', isLoggedIn, function(req, res, next) {
+  res.render("reset-password",{user : req.user});
+});
+      
+router.post("/reset-password/:id", isLoggedIn, async function (req, res, next) {
+  try {
+    await req.user.changePassword(
+      req.body.oldpassword,
+      req.body.newpassword
+    );
+    req.user.save();
+    res.redirect(`/update-user/${req.user._id}`);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated){
+    next()
+  }
+  else{
+    res.redirect("/login")
+  }
+}
+
+
+
 module.exports = router;
+
 
